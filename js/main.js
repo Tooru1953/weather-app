deaf35dad535d1d58b542a691dfa006db90d8131
Stanislav <datura1953@mail.ru>
2024-01-10 12:26:06 +0700

    final

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const apikey = "734be81a85495cde9cc8e4aa13e46c38";
+const weatherDataEl = document.getElementById("weather-data");
+const cityInputEl = document.getElementById("city-input");
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = cityInputEl.value;
+  getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const detailes = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind speed: ${data.wind.speed} m/s`,
+    ];
+
+    //   weatherDataEl.querySelector(".icon").innerHTML = `<img
+    // 	src="htpp://openweathermap.org/img/wn/${icon}.png"
+    // 	alt="Weather icon"
+    // />`;
+
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+
+    weatherDataEl.querySelector(".description").textContent = description;
+
+    weatherDataEl.querySelector(".detailes").innerHTML = detailes
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+
+    weatherDataEl.querySelector(".temperature").textContent = "";
+
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try agin later";
+
+    weatherDataEl.querySelector(".detailes").innerHTML = "";
+  }
+}
